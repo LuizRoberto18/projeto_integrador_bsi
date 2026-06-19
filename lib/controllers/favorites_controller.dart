7581740113b8de7ec8ffeb6147459d373fb931f5
Pablo Franciolly <126625031+pablohcff@users.noreply.github.com>
2026-06-19 11:02:08 -0300

    inserção de favoritos

--- lib/controllers/favorites_controller.dart
+++ lib/controllers/favorites_controller.dart
@@ -1,3 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
-class FavoritesController extends GetxController {}
+import 'auth_controller.dart';
+
+class FavoritesController extends GetxController {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final AuthController _auth = Get.find<AuthController>();
+
+  final RxList<String> favorites = <String>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    ever(_auth.currentUser, (_) => _loadFavorites());
+    _loadFavorites();
+  }
+
+  void _loadFavorites() {
+    final user = _auth.currentUser.value;
+    if (user != null) {
+      favorites.assignAll(user.favorites);
+    }
+  }
+
+  bool isFavorite(String locationId) => favorites.contains(locationId);
+
+  Future<void> toggleFavorite(String locationId) async {
+    final user = _auth.currentUser.value;
+    if (user == null) return;
+
+    if (favorites.contains(locationId)) {
+      favorites.remove(locationId);
+    } else {
+      favorites.add(locationId);
+    }
+
+    await _db.collection('users').doc(user.id).update({
+      'favorites': favorites.toList(),
+    });
+  }
+}
