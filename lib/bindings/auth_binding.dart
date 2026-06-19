1e8cc352c7ecc011e8859a889503ed9271123ff1
Pablo Franciolly <126625031+pablohcff@users.noreply.github.com>
2026-06-19 10:25:53 -0300

    implementando classe de auth_binding

--- lib/bindings/auth_binding.dart
+++ lib/bindings/auth_binding.dart
@@ -1,8 +1,9 @@
 import 'package:get/get.dart';
+import '../controllers/onboarding_controller.dart';
 
 class AuthBinding extends Bindings {
   @override
   void dependencies() {
-    // TODO: implement dependencies
+    Get.lazyPut(() => OnboardingController());
   }
 }
