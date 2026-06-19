1f9e20ac006e351364abd3febbf1dbc52c4cd102
Pablo Franciolly <126625031+pablohcff@users.noreply.github.com>
2026-06-19 11:04:18 -0300

    Add onboarding controller
    Implemented onboarding functionality with slide tracking and completion logic

--- lib/controllers/onboarding_controller.dart
+++ lib/controllers/onboarding_controller.dart
@@ -1,3 +1,25 @@
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class OnboardingController extends GetxController {}
+import '../routes/app_routes.dart';
+
+class OnboardingController extends GetxController {
+  static const _key = 'onboarding_seen';
+
+  final RxInt currentSlide = 0.obs;
+
+  static Future<bool> hasSeenOnboarding() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_key) ?? false;
+  }
+
+  void next() {
+    if (currentSlide.value < 2) currentSlide.value++;
+  }
+
+  Future<void> complete({bool goToRegister = false}) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_key, true);
+    Get.offAllNamed(AppRoutes.login, arguments: goToRegister ? {'mode': 'register'} : null);
+  }
+}
