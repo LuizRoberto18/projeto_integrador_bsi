b75e397e46c64322547a425959b7376c252d495a
Pablo Franciolly <126625031+pablohcff@users.noreply.github.com>
2026-06-19 10:44:00 -0300

    Update initial_binding.dart

--- lib/bindings/initial_binding.dart
+++ lib/bindings/initial_binding.dart
@@ -1,8 +1,7 @@
 import 'package:get/get.dart';
-
 import '../controllers/auth_controller.dart';
-import '../controllers/favorites_controller.dart';
 import '../controllers/settings_controller.dart';
+import '../controllers/favorites_controller.dart';
 
 class InitialBinding extends Bindings {
   @override
