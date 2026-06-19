c0eb5bd20720187383b6376a4141d757ca43254e
Pablo Franciolly <126625031+pablohcff@users.noreply.github.com>
2026-06-19 10:31:45 -0300

    implementando homebindings

--- lib/bindings/home_binding.dart
+++ lib/bindings/home_binding.dart
@@ -1,8 +1,11 @@
 import 'package:get/get.dart';
+import '../controllers/location_controller.dart';
+import '../controllers/review_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    // TODO: implement dependencies
+    Get.lazyPut(() => LocationController());
+    Get.lazyPut(() => ReviewController());
   }
 }
